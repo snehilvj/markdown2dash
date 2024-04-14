40ccc07656f5ccf05837cc08db87a9de77273f74
Snehil Vijay <snehilvj@outlook.com>
2024-04-14 15:14:19 +0530

    Add runner script

--- render.sh
+++
@@ -1,5 +0,0 @@
-python -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-pip install markdown2dash
- gunicorn example.app:server
---
+++ render/run.sh
@@ -0,0 +1,1 @@
+gunicorn example.app:server
